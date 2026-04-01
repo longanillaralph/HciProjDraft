d4bc68ca27a39020d64f054b9921a94a22150fb0
EYAN <EYAN@LAPTOP-Q7I12CCV>
2026-04-01 17:35:36 +0800

    junel = red

--- red.Designer.vb
+++ red.Designer.vb
@@ -69,7 +69,7 @@ Partial Class red
         ' 
         ' Button1
         ' 
-        Button1.Location = New Point(362, 282)
+        Button1.Location = New Point(383, 252)
         Button1.Name = "Button1"
         Button1.Size = New Size(75, 71)
         Button1.TabIndex = 3
--- red.vb
+++ red.vb
@@ -1,3 +1,6 @@
 ﻿Public Class red
-
+    Private Sub red_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim junel As String
+        junel = "red"
+    End Sub
 End Class
