42fedb723fd13dcdf583520622dbefa964641192
EYAN <EYAN@LAPTOP-Q7I12CCV>
2026-04-01 17:44:51 +0800

    junel naging blue

--- red.vb
+++ red.vb
@@ -2,5 +2,8 @@
     Private Sub red_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim junel As String
         junel = "red"
+
+
+        junel = "blue"
     End Sub
 End Class
